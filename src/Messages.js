0644813800fca7cbb824a960e80bd6b024918e8a
vagrant <vagrant@vagrant.vm>
2021-08-10 02:51:27 +0000

    got basic nav bar and message setup

--- src/Messages.js
+++ src/Messages.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
-import './Messages.css'
+import './Messages.css';
+import MessagesNavbar from './MessagesNavbar';
 
 class Messages extends Component {
     render() {
-        return (
-            <body>
-                <header>
-                    <h1>Chat</h1>
-                </header>
-            </body>
-
+        return ( 
+        <div className="messages-screen">
+            <MessagesNavbar />
+            <ul className="messages-list">
+                <li className="message-content">
+                    <p>Sender's Image</p>
+                    <ul className="message-info">
+                        <h3>Sender's Name</h3>
+                        <div className="message-sub-info"><p>Icon</p><p>Message Status</p><p>Time ago</p><p>Streak</p></div>
+                    </ul>
+                </li>
+            </ul>
+        </div>
         );
     }
 }
