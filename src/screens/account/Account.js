092c7192623859990425bc86c451809f463febd2
jfangwang <qbs1864@gmail.com>
2022-03-04 21:09:18 -0500

    added some css to account

--- src/screens/account/Account.js
+++ src/screens/account/Account.js
@@ -23,26 +23,31 @@ export default class Account extends Component {
     return (
       <main className="main">
         <div className="section info">
-          <h1>Me</h1>
-          <div className="row">
-            <img src="https://cdn3.iconfinder.com/data/icons/dashboard-ui-element/32/Dashboard_icon_design_expanded-28-512.png" />
-            <h1>{userDoc.name}</h1>
-            <i><h5>{userDoc.username}</h5></i>
+          <div className="row ">
+            <div className="pic-container">
+              <img src={userDoc['profile_pic_url']} alt="" />
+            </div>
+            <div className="col">
+              <h1>{userDoc.name}</h1>
+              <i><h5>{userDoc.username}</h5></i>
+            </div>
           </div>
-          <div className="row">
+          <div className="col">
             <h5>Total: {userDoc.sent + userDoc.received}</h5>
             <h5>Sent: {userDoc.sent}</h5>
             <h5>Received: {userDoc.received}</h5>
           </div>
         </div>
-        <div className="section">
-          <h1>Friends</h1>
-          <div className="col">
-            {Object.keys(userDoc.friends).sort().map((key) => (
-              <h3>{userDoc.friends[key]['name']}</h3>
-            ))}
+        {Object.keys(userDoc.friends).filter(item => item !== userDoc['email']).length === 0 ? null :
+          <div className="section">
+            <h1>Friends</h1>
+            <div className="col">
+              {Object.keys(userDoc.friends).filter(item => item !== userDoc['email']).sort().map((key) => (
+                <h3>{userDoc.friends[key]['name']}</h3>
+              ))}
+            </div>
           </div>
-        </div>
+        }
         <div className="section snapMap">
           <h1>Snap Map</h1>
           <img src="https://media.npr.org/assets/img/2017/06/30/snapchat-world_custom-7763b62a81588d5def16ef6335a573d94e0dc908.jpg" />
@@ -52,9 +57,9 @@ export default class Account extends Component {
         </div>
         <div className="section">
           {userDoc['name'] === 'Guest' ?
-            <button onClick={this.signIn}><h2>Login</h2></button>
+            <button onClick={this.signIn} className="login"><h2>Login</h2></button>
             :
-            <button onClick={this.signOut}><h2>Logout</h2></button>
+            <button onClick={this.signOut} className="logout"><h2>Logout</h2></button>
           }
         </div>
       </main>
