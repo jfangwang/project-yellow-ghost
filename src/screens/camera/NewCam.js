00485cef6294474d63134574c203ede9c199a940
jfangwang <qbs1864@gmail.com>
2022-03-18 12:03:00 -0400

    fixed mobile landscape camera mode

--- src/screens/camera/NewCam.js
+++ src/screens/camera/NewCam.js
@@ -13,22 +13,23 @@ export default function NewCam({ index, height, width, flipCamCounter, disableNa
   const [img, setImg] = useState(null);
   const [screen, setScreen] = useState("camera");
   const [enabled, setEnabled] = useState(false);
+  const [vidLoaded, setVidLoaded] = useState(false);
   const desktopConstraints = {
     audio: false,
     video: {
       facingMode: "user",
       width: { min: 720 * 0.5625, ideal: 1920 * 0.5625, max: 3840 * 0.5625 },
       height: { min: 720, ideal: 1920, max: 3840 },
-      aspectRatio: 9.5/16,
+      aspectRatio: 9.5 / 16,
     }
   }
-  const mobileConstraints = {
+  var mobileConstraints = {
     audio: false,
     video: {
       facingMode: flipCamCounter % 2 === 0 ? "user" : "environment",
       height: { min: 1280 / width * height, ideal: 1920 / width * height },
       width: { min: 1280, ideal: 1920 },
-      aspectRatio: height/width,
+      aspectRatio: height / width,
     }
   }
 
@@ -37,6 +38,7 @@ export default function NewCam({ index, height, width, flipCamCounter, disableNa
   });
 
   function activateCam() {
+    setVidLoaded(false)
     document.querySelector(".camOverlay").classList.remove("fadeIn")
     document.querySelector(".camOverlay").classList.add("loading")
     console.log("Camera Activated")
@@ -53,10 +55,41 @@ export default function NewCam({ index, height, width, flipCamCounter, disableNa
       })
   }
 
+  function fixOrientation() {
+    if (isMobile && width > height) {
+      mobileConstraints = {
+        audio: false,
+        video: {
+          facingMode: flipCamCounter % 2 === 0 ? "user" : "environment",
+          height: { min: 1280 / width * height, ideal: 1920 / width * height },
+          width: { min: 1280, ideal: 1920 },
+          aspectRatio: 9.5 / 16,
+        }
+      }
+      if (screen === "camera") {
+        activateCam()
+      }
+    } else {
+      mobileConstraints = {
+        audio: false,
+        video: {
+          facingMode: flipCamCounter % 2 === 0 ? "user" : "environment",
+          height: { min: 1280 / width * height, ideal: 1920 / width * height },
+          width: { min: 1280, ideal: 1920 },
+          aspectRatio: height / width,
+        }
+      }
+      if (screen === "camera") {
+        activateCam()
+      }
+    }
+  }
+
   useEffect(() => {
     activateCam()
     const video = document.querySelector("video");
     video.addEventListener("loadeddata", function () {
+      setVidLoaded(true)
       document.querySelector(".camOverlay").classList.remove("loading")
       document.querySelector(".camOverlay").classList.add("fadeIn")
     });
@@ -64,113 +97,119 @@ export default function NewCam({ index, height, width, flipCamCounter, disableNa
 
   useEffect(() => {
     if (stream !== undefined) {
-      activateCam()
+      if (isMobile) {
+        fixOrientation()
+      } else {
+        activateCam()
+      }
     }
   }, [flipCamCounter])
 
   useEffect(() => {
-    if (isMobile && height) {
-
-    }
+    fixOrientation()
   }, [height, width])
 
-  useEffect(() => {
+useEffect(() => {
 
-    if (stream !== undefined) {
-      if (index === 1 && screen === "camera") {
-        activateCam()
+  if (stream !== undefined) {
+    if (index === 1 && screen === "camera") {
+      if (isMobile) {
+        fixOrientation()
       } else {
-        stream.getTracks().forEach(function (track) {
-          track.stop();
-        });
+        activateCam()
       }
-    }
-  }, [index, screen])
-
-  function capture() {
-    const canvas = document.querySelector('#canvasCam');
-    const video = document.querySelector("#cam");
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-    if (isMobile && flipCamCounter % 2 === 0) {
-      canvas.getContext("2d").scale(-1, 1);
-      canvas.getContext("2d").drawImage(video, canvas.width * -1, 0);
-    }
-    if (enabled) {
-      canvas.getContext("2d").drawImage(video, 0, 0);
-      setImg(canvas.toDataURL("image/png"));
     } else {
-      setImg(null);
+      stream.getTracks().forEach(function (track) {
+        track.stop();
+      });
     }
-    setScreen('captured');
-    disableNavFootSlide(true);
   }
-  function changedToSend() { setScreen('send') }
-  function backToCapture() { setScreen('captured') }
-  function save() { close() }
-  function close() {
-    setImg(null);
-    setScreen('camera');
-    disableNavFootSlide(false);
-  }
-  function sent() {
-    setImg(null);
-    setScreen('camera');
-    disableNavFootSlide(false);
-    changeToIndex(0);
-  }
+}, [index, screen])
 
-  const mirrorStyle = { transform: "scaleX(-1)" }
-  return (
-    <>
-      <video
-        autoPlay
-        playsInline
-        style={(flipCamCounter % 2 === 0 && isMobile) || (!isMobile) ? mirrorStyle : null}
-        height="100%"
-        width="100%"
-        id="cam"
-      />
-      <canvas
-        id="canvasCam"
-        className='canvas'
-        style={{ position: "absolute" }}
+function capture() {
+  const canvas = document.querySelector('#canvasCam');
+  const video = document.querySelector("#cam");
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  if (isMobile && flipCamCounter % 2 === 0) {
+    canvas.getContext("2d").scale(-1, 1);
+    canvas.getContext("2d").drawImage(video, canvas.width * -1, 0);
+  }
+  if (enabled) {
+    canvas.getContext("2d").drawImage(video, 0, 0);
+    setImg(canvas.toDataURL("image/png"));
+  } else {
+    setImg(null);
+  }
+  setScreen('captured');
+  disableNavFootSlide(true);
+}
+function changedToSend() { setScreen('send') }
+function backToCapture() { setScreen('captured') }
+function save() { close() }
+function close() {
+  setImg(null);
+  setScreen('camera');
+  disableNavFootSlide(false);
+}
+function sent() {
+  setImg(null);
+  setScreen('camera');
+  disableNavFootSlide(false);
+  changeToIndex(0);
+}
+
+const mirrorStyle = { transform: "scaleX(-1)" }
+return (
+  <>
+    <video
+      autoPlay
+      playsInline
+      style={(flipCamCounter % 2 === 0 && isMobile) || (!isMobile) ? mirrorStyle : null}
+      height="100%"
+      width="100%"
+      id="cam"
+    />
+    <canvas
+      id="canvasCam"
+      className='canvas'
+      style={{ position: "absolute" }}
+      height={height}
+      width={width}
+    />
+
+    <div
+      className="camOverlay"
+      style={{ position: "absolute", height: height, width: width }}
+      {...double_tap}
+    >
+      <div>
+      </div>
+      <div>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <button className="capture-button" onClick={vidLoaded ? capture : null}></button>
+        </div>
+        <Footer type="relative" />
+      </div>
+    </div>
+
+    {screen === "captured" &&
+      <Capture
         height={height}
         width={width}
+        img={img}
+        close={close}
+        changedToSend={changedToSend}
+        save={save}
+        backToCapture={backToCapture}
+        userDoc={userDoc}
+        setUserDoc={setUserDoc}
+        sent={sent}
+        toggleSnapShot={toggleSnapShot}
       />
+    }
 
-      <div
-        className="camOverlay"
-        style={{ position: "absolute", height: height, width: width }}
-        {...double_tap}
-      >
-        <div>
-        </div>
-        <div>
-          <div style={{ display: "flex", justifyContent: "center" }}>
-            <button className="capture-button" onClick={capture}></button>
-          </div>
-          <Footer type="relative" />
-        </div>
-      </div>
+  </>
 
-      {screen === "captured" &&
-        <Capture
-          height={height}
-          width={width}
-          img={img}
-          close={close}
-          changedToSend={changedToSend}
-          save={save}
-          backToCapture={backToCapture}
-          userDoc={userDoc}
-          setUserDoc={setUserDoc}
-          sent={sent}
-          toggleSnapShot={toggleSnapShot}
-        />
-      }
-
-    </>
-
-  )
+)
 }
