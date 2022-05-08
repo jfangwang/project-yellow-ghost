97483762d62faf45ca6a573dd0bd69db5d0a8cc5
jfangwang <qbs1864@gmail.com>
2022-05-08 01:28:45 -0400

    discover page has mock data

--- src/Screens/Discover/Discover.js
+++ src/Screens/Discover/Discover.js
@@ -2,6 +2,12 @@ import React, {Component} from 'react';
 import Navbar from '../../Components/Navbar/Navbar';
 import PropTypes from 'prop-types';
 import {MetaTags} from 'react-meta-tags';
+import johnnyDepp from '../../Assets/images/discover/johnnyDepp.jpg';
+import foxNews from '../../Assets/images/discover/foxNews.png';
+import snl from '../../Assets/images/discover/snl.jpeg';
+import nba from '../../Assets/images/discover/nba.jpeg';
+import styles from './Discover.module.css';
+
 
 /**
  * @export
@@ -24,8 +30,33 @@ export class Discover extends Component {
           />
         </MetaTags>
         <div>
-          <Navbar opacity={0} position="relative" />
-          <h1>Discover</h1>
+          <Navbar opacity={0} position="relative"/>
+          <div className={styles.discoverContent}>
+            <div className={styles.discoverItem}>
+              <img
+                alt="placeholder"
+                src={johnnyDepp}
+              />
+            </div>
+            <div className={styles.discoverItem}>
+              <img
+                alt="placeholder"
+                src={foxNews}
+              />
+            </div>
+            <div className={styles.discoverItem}>
+              <img
+                alt="placeholder"
+                src={snl}
+              />
+            </div>
+            <div className={styles.discoverItem}>
+              <img
+                alt="placeholder"
+                src={nba}
+              />
+            </div>
+          </div>
         </div>
       </>
     );
